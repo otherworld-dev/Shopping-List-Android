a945d8228d48849442b12706e97d77bff4350a72
Adam Morgan <adam@otherworld.dev>
2026-07-19 18:48:40 +0100

    chore(release): bump to 0.2.0 (versionCode 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "dev.otherworld.shoppinglist"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
