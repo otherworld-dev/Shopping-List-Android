31c62c3518ffc405f142f89edb0f56ae98ab4556
Adam Morgan <adam@otherworld.dev>
2026-09-25 21:19:57 +0100

    chore(release): bump to 0.5.0 (versionCode 5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "dev.otherworld.shoppinglist"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
